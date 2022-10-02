fff196dec95c26d58955964e3ae6a67d5eabf517
Ruslan <ruslanvnikolov@gmail.com>
2022-10-02 14:24:50 +0300

    Initial commit, mongo DB, repos, models, Avatar upload and retrieval

---
+++ src/main/java/io/ruslan/top100crypto/Top100CryptoApplication.java
@@ -0,0 +1,13 @@
+package io.ruslan.top100crypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Top100CryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Top100CryptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ruslan/top100crypto/Top100CryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ruslan.top100crypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Top100CryptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
